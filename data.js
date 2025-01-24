3e1c5ea3911a06d9edaec11032edb9aa98625629
HDN18 <hanif.dwi509@smk.belajar.id>
2025-01-24 14:38:45 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,289 @@
+
+const teams = [ 
+    {
+        "name": "Muhammad Khaidir Fahram",
+        "email": "khaidir@fahram.com",
+        "image": "https://fahram.com//img/profile-photos/jzZj0jPuEKTWwY9a8lxo7pQhA29zxFpVQd7xl4BZ.jpg",
+        
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://instagram.com/khaidir.fahram"
+            },
+            {
+                "name" : "facebook",
+                "link" : "https://facebook.com/khaidir.fahram"
+            },
+
+            {
+                "name" : "globe",
+                "link" : "https://fahram.com/"
+            }
+        ]
+    },
+
+    {
+        "name": "Achmad Maulana Ichsan",
+        "email": "ichsanachmadmaulana@gmail.com",
+        "image": "maul.jpg",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://instagram.com/a.iichsan"
+            },
+
+            {
+                "name" : "globe",
+                "link" : "https://icsan.my.id/"
+            },
+
+            {
+                "name" : "whatsapp",
+                "link" : "https://wa.me/6287819845197"
+            }
+        ]
+    },
+
+    {
+        "name": "Marcella Setyaning Putri",
+        "email": "msetyaningputri@gmail.com",
+        "image": "sela.jpg",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/mqrcelllaaaa?igsh=MTk5aWhyYXMybXN5bA== "
+            },
+
+            {
+                "name" : "globe",
+                "link" : "https://marcellasetyaningputri.my.id/"
+            },
+
+            {
+                "name" : "whatsapp",
+                "link" : "https://wa.me/6283167716324"
+            }
+        ]
+    },
+
+    {
+        "name": "Muhammad Fikri Firdaus",
+        "email": "mfikrif06@gmail.com",
+        "image": "fikri.jpg",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/m_f1kr1",
+            },
+
+            {
+                "name" : "globe",
+                "link" : "https://fikriy.my.id/"
+            },
+
+            {
+                "name" : "whatsapp",
+                "link" : "https://wa.me/"
+            }
+        ]
+    },
+
+    {
+        "name": "Muhammad Rivanda Ramdhani",
+        "email": "ramdhanirivanda@gmail.com ",
+        "image": "https://tse4.mm.bing.net/th?id=OIP.g6sTdGfbyN2DffeFBszqqQAAAA&pid=Api&P=0&h=220",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/ramdhanirivanda/?igsh=ZjM4ODNla2VmcG1j"
+            },
+
+            {
+                "name" : "globe",
+                "link" : "https://marcellasetyaningputri.my.id/"
+            },
+
+            {
+                "name" : "whatsapp",
+                "link" : "https://wa.me/"
+            }
+        ]
+    },
+
+    {
+        "name": "Naufal Laudza",
+        "email": "naufallaudza194@gmail.com",
+        "image": "nopal.jpg",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/naufalaudzaa"
+            },
+
+            {
+                "name" : "globe",
+                "link" : "https://naufal19.my.id/"
+            },
+
+            {
+                "name" : "whatsapp",
+                "link" : "https://wa.me/"
+            }
+        ]
+    },
+
+    {
+        "name": "Rozak Al Baidawi",
+        "email": "rozakalbaidawi@gmail.com",
+        "image": "rojak.jpg",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/_rzkalbdwi/"
+            },
+
+            {
+                "name" : "globe",
+                "link" : "https://rozak19.my.id/"
+            },
+
+            {
+                "name" : "whatsapp",
+                "link" : "https://wa.me/"
+            }
+        ]
+    },
+
+    {
+        "name": "Muhammad Sandi Nourman",
+        "email": "sandi@gmail.com",
+        "image": "https://raw.githubusercontent.com/sandinourman123/sandinourman123.github.io/refs/heads/main/sandi.jpeg",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : ""
+            },
+            {
+                "name" : "globe",
+                "link" : "https://sandinourman.my.id/"
+            },
+
+            {
+                "name" : "whatsapp",
+                "link" : "https://wa.me/"
+            }
+        ]
+    },
+
+    {
+        "name": "Hanif Dwi Novario",
+        "email": "hanifdn05@gmail.com",
+        "image": "pantai.jpg",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://instagram.com/hnfdwnvr/"
+            },
+            
+            {
+                "name" : "globe",
+                "link" : "https://hanifdwi.my.id/"
+            },
+
+            {
+                "name" : "whatsapp",
+                "link" : "https://wa.me/62895415834791"
+            }
+        ]
+    },
+
+    {
+        "name": "Abdulrahman Arafah Sunarto",
+        "email": "fahganindo12@gmail.com",
+        "image": "https://tse4.mm.bing.net/th?id=OIP.g6sTdGfbyN2DffeFBszqqQAAAA&pid=Api&P=0&h=220",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/arafah.abdulrahman_/"
+            },
+            
+            {
+                "name" : "globe",
+                "link" : "https://fahganindo4life.my.id/"
+            },
+
+            {
+                "name" : "whatsapp",
+                "link" : "https://wa.me/"
+            }
+        ]
+    },
+
+    {
+        "name": "Rheina Wardiana Hafiroh",
+        "email": "rheinawardiana317@gmail.com ",
+        "image": "reina.jpg",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/na_rrrrr44/"
+            },
+            
+            {
+                "name" : "globe",
+                "link" : "https://rheinawardianahafiroh.my.id/"
+            },
+
+            {
+                "name" : "whatsapp",
+                "link" : "https://wa.me/"
+            }
+        ]
+    },
+
+    {
+        "name": "Siti Aisyah",
+        "email": "siti.aisyah.2021@gmail.com",
+        "image": "https://tse4.mm.bing.net/th?id=OIP.g6sTdGfbyN2DffeFBszqqQAAAA&pid=Api&P=0&h=220",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : ""
+            },
+            
+            {
+                "name" : "globe",
+                "link" : "https://kaisyah.my.id/"
+            },
+
+             {
+                "name" : "whatsapp",
+                "link" : "https://wa.me/"
+            }
+        ]
+    },
+
+    {
+        "name": "Fredo Pradana",
+        "email": "Fredopradana72@gmail.com",
+        "image": "https://tse4.mm.bing.net/th?id=OIP.g6sTdGfbyN2DffeFBszqqQAAAA&pid=Api&P=0&h=220",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : ""
+            },
+            
+            {
+                "name" : "globe",
+                "link" : "https://kaisyah.my.id/"
+            },
+
+            {
+                "name" : "whatsapp",
+                "link" : "https://wa.me/"
+            }
+        ]
+    },
+
+    
+]
